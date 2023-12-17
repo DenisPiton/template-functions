c5ba024cb8c238403ac966bcabfb5f4d699de0ef
Denis <denfill008@mail.ru>
2023-12-17 16:19:25 +0300

    finish

--- template functions/first.cpp
+++ template functions/first.cpp
@@ -26,7 +26,31 @@ T minimum(T* Arr, int a) {
 	}
 	return buf;
 }
+template <typename T>
+void bubbleSort(T* list, T listLength)
+{
+	while (listLength--)
+	{
+		bool swapped = false;
 
+		for (int i = 0; i < listLength; i++)
+		{
+			if (list[i] > list[i + 1])
+			{
+				swap(list[i], list[i + 1]);
+				swapped = true;
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+}
+template <typename T>
+void swap(T* list, int index, T item)
+{
+	list[index] = item;
+}
 int main()
 {
 	int Array[10]{ 1,2,11,4,5,6,7,8,9,10 };
@@ -34,6 +58,13 @@ int main()
 	int a = 10;
 	maximum(Array,a);
 	minimum(Array, a);
+	bubbleSort(Array, a);
+	swap(Array, 3, 100);
+	for (int i : Array)
+	{
+		cout << i<<" ";
+	}
+	
 	
 	
 }
